b954d48eae97e04599b05880887ea2af97db23d6
Asad Rizvi <asadrzv@outlook.com>
2023-05-20 00:45:43 -0400

    Refactor

--- ChatAIBot/Views/ImageView.swift
+++ ChatAIBot/Views/ImageView.swift
@@ -11,6 +11,9 @@ struct ImageView: View {
     let imageUrl: String
     let dimensions: Double // REPLACE HARDCODED DIMENSIONS WITH DYNAMIC SIZING
     
+    // Specify animation between phase changes
+    private let transaction: Transaction = .init(animation: .linear)
+        
     init(imageUrl: String, dimensions: Double) {
         self.imageUrl = imageUrl
         self.dimensions = dimensions
@@ -18,36 +21,39 @@ struct ImageView: View {
     
     var body: some View {
         // Use AsyncImage to fetch image from URL and automatically cache it
-        AsyncImage(url: URL(string: imageUrl)) { phase in
+        AsyncImage(url: URL(string: imageUrl), transaction: transaction) { phase in
             // Transaction phase parameter to handle errors loading image
             switch phase {
             // Placeholder loading image
             case .empty:
                 ProgressView()
-                    .frame(width: dimensions, height: dimensions)
-                    .background(.thinMaterial)
             // Valid image
             case .success(let image):
                 image
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: dimensions, height: dimensions)
-                    .background(.thinMaterial)
             // Error fetching image from url
             case .failure:
-                Text("⚠️ Oh no! Something went wrong fetching the image 🖼️!")
-                    .frame(width: dimensions, height: dimensions)
-                    .background(.thinMaterial)
+                Text("⚠️ Oh no! I can't find the image you're looking for 🖼️")
+                    .multilineTextAlignment(.center)
             // Error unknown
             @unknown default:
                 Text("Not sure what happened 😕. Try restarting the app.")
+                    .multilineTextAlignment(.center)
             }
         }
+        .frame(width: dimensions, height: dimensions)
+        .background(.thinMaterial)
     }
 }
 
 struct ImageView_Previews: PreviewProvider {
     static var previews: some View {
+        // Empty image URL
+        ImageView(imageUrl: "", dimensions: 200)
+        // Successfully fetched image
         ImageView(imageUrl: Constants.sampleImageURL, dimensions: 200)
+        // Error fetching image from URL
+        ImageView(imageUrl: "error", dimensions: 200)
     }
 }
